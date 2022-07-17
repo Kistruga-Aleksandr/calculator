7bed24f9b775824155b04e330a9516d5e3fa3d76
Александр <a@kistruga.ru>
2022-07-17 18:02:10 +0400

    ДЗ Вводное в Maven и Spring Boot + web Исправил

--- src/main/java/pro/sky/calculator/CalculatorController.java
+++ src/main/java/pro/sky/calculator/CalculatorController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class CalculatorController{
+public class CalculatorController {
     private final Calculator calculator;
 
     public CalculatorController(Calculator calculator) {
@@ -13,25 +13,51 @@ public class CalculatorController{
     }
 
     @GetMapping(value = "/plus")
-    public String addition(@RequestParam int num1,
-                                   @RequestParam int num2) {
-        return calculator.addition(num1, num2);
-    }
-    @GetMapping(value ="/minus" )
-    public String subtraction(@RequestParam int num1,
-                                   @RequestParam int num2) {
-        return calculator.subtraction(num1, num2);
-    }
-
-    @GetMapping(value ="/multiply" )
-    public String multiplication(@RequestParam int num1,
-                                   @RequestParam int num2) {
-        return calculator.multiplication(num1, num2);
+    public String addition(@RequestParam(required = false) Integer num1,
+                           @RequestParam(required = false) Integer num2) {
+        if (areParamsPresent(num1, num2)) {
+            return calculator.addition(num1, num2);
+        } else {
+            return "Не передан параметр ";
+        }
     }
 
-    @GetMapping(value ="/divide" )
-    public String division(@RequestParam int num1,
-                                   @RequestParam int num2) {
-        return calculator.division(num1, num2);
+    @GetMapping(value = "/minus")
+    public String subtraction(@RequestParam(required = false) Integer num1,
+                              @RequestParam(required = false) Integer num2) {
+        if (areParamsPresent(num1, num2)) {
+            return calculator.subtraction(num1, num2);
+        } else {
+            return "Не передан параметр ";
+        }
+    }
+
+    @GetMapping(value = "/multiply")
+    public String multiplication(@RequestParam(required = false) Integer num1,
+                                 @RequestParam(required = false) Integer num2) {
+        if (areParamsPresent(num1, num2)) {
+            return calculator.multiplication(num1, num2);
+        } else {
+            return "Не передан параметр ";
+        }
+    }
+
+    @GetMapping(value = "/divide")
+    public String division(@RequestParam(required = false) Integer num1,
+                           @RequestParam(required = false) Integer num2) {
+        if (areParamsPresent(num1, num2)) {
+            if (num2 == 0) {
+                return " На 0 делить нельзя ";
+            }
+                return calculator.division(num1, num2);
+            }
+            else {
+                return "Не передан параметр ";
+            }
+
+    }
+
+    public boolean areParamsPresent(Integer num1, Integer num2) {
+        return num1 != null && num2 != null;
     }
 }
